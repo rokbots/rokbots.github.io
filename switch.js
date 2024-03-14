d490ad497a99992ec6e323b87ad5cb114bca9eb0
Tong Vuu <silverwolf@ceh.vn>
2024-03-14 21:45:20 +0700

    Vi and E language

---
+++ switch.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+const myMap = new Map([
+    [document.getElementById("vnflag"), "index.html"],
+    [document.getElementById("enflag"), 'index_en.html'],
+    ]);
+$(".fa-beat-fade").click(function(e) {
+    var page = myMap.get(e.target);
+    document.location.href = page;
+});
